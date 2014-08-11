167fd3cb5dbde01388a568333078cdde02d9eba2
Alexei Chekulaev <alexei.chekulaev@blinkreaction.com>
2014-08-11 00:01:46 -0400

    Tint settings

---
+++ main.js
@@ -0,0 +1,7 @@
+TIWindow.create({
+	url:'index.html',
+	width:800,
+	height:500,
+	frame:'normal', // can also be transparent or frameless
+	glass:true // do not render a background for a more native look.
+})
