2e4abac65a51e950b730bca959bfe0ce7116865c
Alexei Chekulaev <alexei.chekulaev@blinkreaction.com>
2014-12-20 10:26:34 -0500

    Version 1.0

--- main.js
+++
@@ -1,7 +0,0 @@
-TIWindow.create({
-	url:'index.html',
-	width:800,
-	height:500,
-	frame:'normal', // can also be transparent or frameless
-	glass:true // do not render a background for a more native look.
-})
